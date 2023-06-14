ec7b45a9b53e2a6a592d07db8317e7dbdae160ca
pmukherjee013 <mukherjee.piyush@mayo.edu>
2023-06-14 17:19:44 +0000

    update of 6-14-23

---
+++ spectrain_proc_img/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='spectrain_proc_img_trainer',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    description='spectrain edge detected image model training application.'
+)
